95d2f275d750b2323f0fa3f4b1f598655d3eb635
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2023-01-23 14:33:06 +0530

    add treelist  showIcon  story

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -121,6 +121,7 @@ const showSearch = (value = true) => getKnob('showSearch', value);
 const showClear = (value = true) => getKnob('showClear', value);
 const highlight = (value = true) => getKnob('highlight', value);
 const showCheckbox = (value = true) => getKnob('showCheckbox', value);
+const showIcon = (value = true) => getKnob('showIcon', value);
 const rangeLabels = value => getKnob('rangeLabels', value);
 const showTooltip = value => getKnob('showTooltip', value, select, false);
 // eslint-disable-next-line no-shadow
@@ -169,6 +170,11 @@ storiesOf('List components/TreeList', module)
 		props: showCheckbox(),
 		components: { BaseTreeList },
 		template: '<base-tree-list :subProps="{ showCheckbox, showFilter: false}"/>',
+	}))
+	.add('with showIcon', () => ({
+		props: showIcon(),
+		components: { BaseTreeList },
+		template: '<base-tree-list :subProps="{ showIcon, showFilter: false}"/>',
 	}));
 
 storiesOf('Range Components/SingleRange', module)
